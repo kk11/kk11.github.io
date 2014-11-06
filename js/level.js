a5e3ab99c973ee7b45c8dbcaefc47f9dbce69771
Kevin Katzer <kk11@inf.ufpr.br>
2014-11-06 15:23:29 -0200

    Mapa

--- js/level.js
+++ js/level.js
@@ -9,7 +9,14 @@ function init() {
 	var mapOptions = {
 		center: new google.maps.LatLng(-25.3812294, -49.2216299),
 		zoom: 17,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		overviewMapControl: false,
+		panControl: false,
+		rotateControl: false,
+		scaleControl: false,
+		zoomControl: false,
+		streetViewControl: false,
+		mapTypeControl: false,
 	}
 
 	var map = new google.maps.Map(mapCanvas,mapOptions);
@@ -17,8 +24,15 @@ function init() {
 	var marker = new google.maps.Marker({
 		map: map,
 		draggable: false,
-		position: new google.maps.LatLng(-25.3812376, -49.2216068)
+		position: new google.maps.LatLng(-25.3812376, -49.2216068),
+		title: 'Rua Calixto de Jesus, 501 - Curitiba'
 	})
+
+	var info = new google.maps.InfoWindow({
+		content: 'Rua Calixto de Jesus, 501 - Curitiba'
+	})
+
+	info.open(map,marker);
 }
 
 google.maps.event.addDomListener(window, 'load', init);
